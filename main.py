245ea7159d5a37bf28bdeceaad110a7039705f89
atrox3d <atrox3d@gmail.com>
2025-03-02 13:33:49 +0100

    add pydantic, jsonschema

--- main.py
+++ main.py
@@ -7,10 +7,36 @@ import typer
 app = typer.Typer(add_completion=False)
 
 @app.command()
-def main(host:str=defaults.REMOTEHOST):
+def main(
+    prompt:str, 
+    host:str=defaults.REMOTEHOST
+):
     try:
         with OllamaServerCtx(host=host):
             client = ollama.Client(host)
+            response = client.chat(
+                model='llama3.2',
+                messages=[
+                    {
+                        'role': 'user',
+                        'content': prompt
+                    },
+                ],
+                # format='json'
+                format= {
+                    "type": "object",
+                    "properties": {
+                        "question": {
+                            "type": "string"
+                        },
+                        "answer": {
+                            "type": "string"
+                        },
+                    },
+                    "required": [ "question", "answer", ] 
+                }
+            )
+            print(response.message.content)
     except TimeoutError as tme:
         print(f'FATAL| {tme}')
 
