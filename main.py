d6ce1be8cb9363c8e06adb9a21c6fc09345b4576
atrox3d <atrox3d@gmail.com>
2025-03-02 13:39:41 +0100

    add system message and jsonschema from pydantic model

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from ollamahelpers.manager import OllamaServerCtx
-from ollamahelpers import defaults
 import ollama
 import typer
+import pydantic
+import jsonschema
 
+
+from ollamahelpers.manager import OllamaServerCtx
+from ollamahelpers import defaults
 
 app = typer.Typer(add_completion=False)
+
+
+class Answer(pydantic.BaseModel):
+    question    : str = pydantic.Field(description='the exact original user question')
+    answer      : str = pydantic.Field(description='the assistant answer to the question')
+
+
 
 @app.command()
 def main(
@@ -18,23 +28,28 @@ def main(
                 model='llama3.2',
                 messages=[
                     {
+                        'role': 'system',
+                        'content': 'answer with the exact user question and the answer'
+                    },
+                    {
                         'role': 'user',
                         'content': prompt
                     },
                 ],
                 # format='json'
-                format= {
-                    "type": "object",
-                    "properties": {
-                        "question": {
-                            "type": "string"
-                        },
-                        "answer": {
-                            "type": "string"
-                        },
-                    },
-                    "required": [ "question", "answer", ] 
-                }
+                # format= {
+                #     "type": "object",
+                #     "properties": {
+                #         "question": {
+                #             "type": "string"
+                #         },
+                #         "answer": {
+                #             "type": "string"
+                #         },
+                #     },
+                #     "required": [ "question", "answer", ] 
+                # }
+                format=Answer.model_json_schema()
             )
             print(response.message.content)
     except TimeoutError as tme:
