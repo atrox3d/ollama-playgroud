82d5c9992242e91d0cf6a1049fed325d1708faaf
atrox3d <atrox3d@gmail.com>
2025-03-02 13:07:18 +0100

    add typer, ollamahelpers

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from ollama-playgroud!")
+from ollamahelpers.manager import OllamaServerCtx
+from ollamahelpers import defaults
+import ollama
+import typer
+
+
+app = typer.Typer(add_completion=False)
+
+@app.command()
+def main(host:str=defaults.REMOTEHOST):
+    try:
+        with OllamaServerCtx(host=host):
+            client = ollama.Client(host)
+    except TimeoutError as tme:
+        print(f'FATAL| {tme}')
 
 
 if __name__ == "__main__":
-    main()
+    app()
---
+++ ollamahelpers/defaults.py
@@ -0,0 +1,7 @@
+HOST            :str   = 'localhost'
+REMOTEHOST      :str   = '192.168.1.10'
+PORT            :int   = 11434
+ENDPOINT        :str   = 'api/generate'
+URL             :str   = f'http://{HOST}:{PORT}/{ENDPOINT}'
+WAIT_SECONDS    :float = .1
+ATTEMPTS        :int   = 10
